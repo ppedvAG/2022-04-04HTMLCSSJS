c036e2fe58b58f8500535a8239bf21e229d4b9de
Marius Sommer <mariuss@ppedv.de>
2022-04-08 15:13:37 +0200

    Kurs beendet

--- scripts/phases.js
+++ scripts/phases.js
@@ -6,12 +6,24 @@ function clicked(e) {
     console.log(`Button ${e.target.value} wurde geklickt.`);
 }
 
-outer.addEventListener("click", e => clicked(e));
+// outer.addEventListener("click", e => clicked(e));
 
-outer.addEventListener("click", e => console.log("outer löst aus"), true);
-inner.addEventListener("click", e => console.log("inner löst aus"), true);
-wrapper.addEventListener("click", e => console.log("wrapper löst aus"), true);
+// outer.addEventListener("click", e => console.log("outer löst aus"), true);
+// inner.addEventListener("click", e => console.log("inner löst aus"), true);
+// wrapper.addEventListener("click", e => console.log("wrapper löst aus"), true);
 
-inner.addEventListener("click", e => console.log("inner löst aus"));
-wrapper.addEventListener("click", e => console.log("wrapper löst aus"));
-document.querySelector("#b").addEventListener("click", e => console.log("Button wurde geklickt"));
+// inner.addEventListener("click", e => console.log("inner löst aus"));
+// wrapper.addEventListener("click", e => console.log("wrapper löst aus"));
+// document.querySelector("#b").addEventListener("click", e => console.log("Button wurde geklickt"));
+
+wrapper.addEventListener(
+    "click", xyz => clicked(xyz)
+)
+
+
+// . im Pfadname heißt such im selben Ordner
+// ./test/datei.js
+// .. geh einen Ornder höher
+// ~ root Ordner also Hauptordner
+
+// C#, pyhton oder manche js. Frameworks
